a1b815b3f70acbc8cc39d8cc96fa8d01dcb73950
fanuelsen@mac.com <fanuelsen@mac.com>
2020-04-02 16:39:39 +0200

    added random dinner-selector

---
+++ random-dinner/dinner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+#https://askubuntu.com/questions/525599/how-to-display-a-random-line-from-a-text-file
+
+FILE=dinners
+
+# get line count for $FILE (simulate 'wc -l')
+lc=0
+while read -r line; do
+ ((lc++))
+done < $FILE
+
+# get a random number between 1 and $lc
+rnd=$RANDOM
+let "rnd %= $lc"
+((rnd++))
+
+# traverse file and find line number $rnd
+i=0
+while read -r line; do
+ ((i++))
+ [ $i -eq $rnd ] && break
+done < $FILE
+
+# output random line
+printf '%s\n' "$line"
